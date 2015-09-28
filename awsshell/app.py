d6f2949958e42de040a8f9822bbf750b801f9623
James Saryerwinnie <js@jamesls.com>
2015-09-28 15:32:54 -0700

    Fix flake8 issues

--- awsshell/app.py
+++ awsshell/app.py
@@ -3,28 +3,18 @@
 Main entry point to the AWS Shell.
 
 """
+import tempfile
+import subprocess
+
 from prompt_toolkit.document import Document
 from prompt_toolkit.shortcuts import create_eventloop
 from prompt_toolkit.buffer import Buffer
-from prompt_toolkit.enums import DEFAULT_BUFFER, SEARCH_BUFFER
-from prompt_toolkit.filters import IsDone, HasFocus, Always, RendererHeightIsKnown, to_cli_filter
-from prompt_toolkit.interface import CommandLineInterface, Application, AbortAction, AcceptAction
+from prompt_toolkit.filters import Always
+from prompt_toolkit.interface import CommandLineInterface, Application
+from prompt_toolkit.interface import AbortAction, AcceptAction
 from prompt_toolkit.key_binding.manager import KeyBindingManager
-from prompt_toolkit.layout import Window, HSplit, VSplit, FloatContainer, Float
-from prompt_toolkit.layout.containers import ConditionalContainer
-from prompt_toolkit.layout.controls import BufferControl, TokenListControl, FillControl
-from prompt_toolkit.layout.dimension import LayoutDimension
-from prompt_toolkit.layout.menus import CompletionsMenu, MultiColumnCompletionsMenu
-from prompt_toolkit.layout.processors import PasswordProcessor, HighlightSearchProcessor, \
-    HighlightSelectionProcessor, ConditionalProcessor
-from prompt_toolkit.layout.prompt import DefaultPrompt
-from prompt_toolkit.layout.screen import Char
-from prompt_toolkit.layout.toolbars import ValidationToolbar, SystemToolbar, ArgToolbar, SearchToolbar
-from prompt_toolkit.layout.utils import explode_tokens
 from prompt_toolkit.utils import Callback
-from pygments.token import Token
 
-from awsshell.compat import text_type
 from awsshell.ui import create_default_layout
 
 
@@ -55,13 +45,13 @@ class AWSShell(object):
                 if text.strip() in ['quit', 'exit']:
                     break
                 if text.startswith('.'):
-                    # These are special commands.  The only one supported for now
-                    # is .edit.
+                    # These are special commands.  The only one supported for
+                    # now is .edit.
                     if text.startswith('.edit'):
-                        # Hardcoded VIM editor for now.  It's for demo purposes!
+                        # TODO: Use EDITOR env var.
                         all_commands = '\n'.join(
                             ['aws ' + h for h in list(self.history)
-                            if not h.startswith(('.', '!'))])
+                             if not h.startswith(('.', '!'))])
                     with tempfile.NamedTemporaryFile('w') as f:
                         f.write(all_commands)
                         f.flush()
@@ -81,14 +71,12 @@ class AWSShell(object):
             u'aws> ', reserve_space_for_menu=True,
             display_completions_in_columns=True)
 
-
     def create_buffer(self, completer, history):
         return Buffer(
             history=history,
             completer=completer,
             complete_while_typing=Always(),
             accept_action=AcceptAction.RETURN_DOCUMENT)
-
 
     def create_application(self, completer, history):
         key_bindings_registry = KeyBindingManager(
@@ -109,13 +97,12 @@ class AWSShell(object):
             key_bindings_registry=key_bindings_registry,
         )
 
-
     def on_input_timeout(self, cli):
         buffer = cli.current_buffer
         document = buffer.document
         text = document.text
-        cli.buffers['clidocs'].reset(initial_document=Document(text, cursor_position=0))
-
+        cli.buffers['clidocs'].reset(
+            initial_document=Document(text, cursor_position=0))
 
     def create_cli_interface(self):
         # A CommandLineInterface from prompt_toolkit
