943c31de9cf2ffa042473a904c66f3b9b01b4684
Jordan Guymon <guymonj@amazon.com>
2016-07-18 17:07:44 -0700

    Basic file prompt and unit tests

--- tests/unit/test_interaction.py
+++ tests/unit/test_interaction.py
@@ -1,8 +1,9 @@
 import mock
 import pytest
 
+from awsshell.utils import InMemoryFSLayer, FileReadError
 from awsshell.interaction import InteractionLoader, InteractionException
-from awsshell.interaction import SimpleSelect, SimplePrompt
+from awsshell.interaction import SimpleSelect, SimplePrompt, FilePrompt
 
 
 @pytest.fixture
@@ -21,6 +22,12 @@ def simple_prompt():
     return SimplePrompt({}, 'Prompt yo')
 
 
+@pytest.fixture
+def temp_fs():
+    mapping = {'/some/path': 'contents'}
+    return InMemoryFSLayer(mapping)
+
+
 def test_loader(loader):
     # Verify the interaction loader instantiates the correct object
     instance = loader.create({'ScreenType': 'SimpleSelect'}, 'Prompt')
@@ -35,6 +42,36 @@ def test_loader_invalid_types(loader):
     with pytest.raises(InteractionException) as ie:
         loader.create({'ScreenType': 'InteractionLoader'}, 'Prompt')
     assert 'Invalid interaction type: InteractionLoader' in str(ie.value)
+
+
+def test_file_prompt():
+    # Test that the file prompt returns the correct path
+    prompt = mock.Mock()
+    file_prompt = FilePrompt({}, 'msg', prompt)
+    prompt.side_effect = ['/some/path', '~']
+    path = file_prompt.get_path()
+    assert path == '/some/path'
+    path = file_prompt.get_path()
+    # Home should be expanded and thus not tilde anymore
+    assert path is not '~'
+
+
+def test_file_prompt_execute(temp_fs):
+    # Test that the file prompt correctly reads files
+    prompt = mock.Mock()
+    file_prompt = FilePrompt({}, 'msg', prompt)
+    prompt.return_value = '/some/path'
+    result = file_prompt.execute({}, fslayer=temp_fs)
+    assert result == 'contents'
+
+
+def test_file_prompt_nonexistent_file(temp_fs):
+    # Test that the file prompt throws an exception when file is not found
+    prompt = mock.Mock()
+    file_prompt = FilePrompt({}, 'msg', prompt)
+    prompt.return_value = '/some/notafile'
+    with pytest.raises(FileReadError):
+        file_prompt.execute({}, fslayer=temp_fs)
 
 
 def test_simple_select():
