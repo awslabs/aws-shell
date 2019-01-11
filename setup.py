e29279cd39feb96d157c631d3e8cf92e3db1bb12
Alexander L. de Goeij <mail@aldg.nl>
2019-01-11 14:27:06 +0100

    latest version for all dependencies, keep prompt-toolkit under v2

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ from setuptools import setup, find_packages
 
 
 requires = [
-    'awscli>=1.16.10,<2.0.0',
-    'prompt-toolkit>=1.0.0,<2.1.0',
-    'boto3>=1.9.0,<3.0.0',
+    'awscli>=1.16.87,<2.0.0',
+    'prompt-toolkit>=1.0.15,<2.0.0',
+    'boto3>=1.9.77,<3.0.0',
     'configobj>=5.0.6,<6.0.0',
     'Pygments>=2.1.3,<3.0.0',
 ]
